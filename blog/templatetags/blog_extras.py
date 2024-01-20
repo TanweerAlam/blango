02a4e705ec1412d6624729401aa08e41a96e706f
Tanweer Alam <tanweeralam1312@gmail.com>
2024-01-20 07:51:24 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from blog.models import Post
 
 # from django.utils.html import escape
 # from django.utils.safestring import mark_safe
@@ -9,6 +10,32 @@ user_model = get_user_model()
 
 register = template.Library()
 
+# Template context
+
+# @register.simple_tag(takes_context=True)
+# def author_details_tag(context):
+#   request = context['request']
+#   current_user = request.user
+#   post = context['post']
+#   author = post.author
+
+#   if author == current_user:
+#     return format_html("<strong>me</strong>")
+  
+#   if author.first_name and author.last_name:
+#     name = f'{author.first_name} {author.last_name}'
+#   else:
+#     name = f'{author.username}'
+
+#   if author.email:
+#     prefix = format_html("<a href='mailto:{}'>", author.email)
+#     suffix = format_html("</a>")
+#   else:
+#     prefix = ""
+#     suffix = ""
+#   return format_html("{}{}{}", prefix, name, suffix)
+
+# Filter
 @register.filter
 def author_details(author):
   if not isinstance(author, user_model):
@@ -22,10 +49,33 @@ def author_details(author):
   if author.email:
     email = author.email
     prefix = format_html('<a href="mailto:{}">', email)
-    suffix = "</a>"
+    suffix = format_html("</a>")
   
   else:
     prefix = ""
     suffix = ""
   
   return format_html('{}{}{}', prefix, name, suffix)
+
+  # Simple tags
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html('</div>')
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[:5]
+  return {"title": "Recent posts", "posts": posts}
